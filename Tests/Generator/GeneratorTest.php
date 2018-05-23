7806e53ad872f30e3cf8dd0ebd380bce715449f8
Julien Deniau <julien.deniau@gmail.com>
2018-05-23 14:13:12 +0200

    no random qrcode

--- Tests/Generator/GeneratorTest.php
+++ Tests/Generator/GeneratorTest.php
@@ -64,6 +64,19 @@ class GeneratorTest extends TestCase
         $this->assertTrue(is_string($generator->generate($options)));
     }
 
+    public function testGenerateNoRandom()
+    {
+        $generator = new Generator();
+
+        $options = [
+            'code' => '1234123412341234',
+            'format' => 'png',
+            'type' => 'qrcode',
+        ];
+
+        $this->assertEquals($generator->generate($options), 'iVBORw0KGgoAAAANSUhEUgAAAGkAAABpAQMAAAADTJ95AAAABlBMVEX///8AAABVwtN+AAAAAXRSTlMAQObYZgAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAPdJREFUOI2tlDsSgzAMRMVQuORI3CzQ5VrMcBF8A5cUnigrgS3SRQYVDI9Cn9UiYkQKkVd5mek+csFM1AP3PhENXhx5Tcgij4rxMQwRhVrxtysahydQxsfk60WNf1Flz0h12cId5LpBDUi3kMW/iLFYA1Ma7lMT8oE8jzJv3OUb9ejQjefyVs4HygK2YRFNyYuqVUkFl7yw1VLIi9O5hSiydzAckrZjJ83FssHcoZoK4cLDGx1au1hFMPDQgmiIk10G4EeqkRPPy8DnkRHMI7zRiOorw6BWqYVcqGZ4WyGCVUL92V2oqewyqIF5I/KhWRS2nQ1T8OIXb3CHNDT2608AAAAASUVORK5CYII=');
+    }
+
     /**
      * @return array
      */
